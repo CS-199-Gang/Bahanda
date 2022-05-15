85ae5cd09574f35ce2dbd6b4db4e89e3fad473a4
Kim Palao <kimfarhantpalao@gmail.com>
2022-05-16 01:52:15 +0800

    Send a request to the API after the second end scene

--- Assets/Scenes/EndScene/EndScene.cs
+++ Assets/Scenes/EndScene/EndScene.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using OVRSimpleJSON;
 
 public class EndScene : MonoBehaviour {
 
@@ -15,23 +18,58 @@ public class EndScene : MonoBehaviour {
     private List<Task> tasks;
     private GameManager gameManager;
 
+    string url = "https://bahanda.kimpalao.com/api";
+    string id;
+
+
     private void Awake() {
         gameManager = FindObjectOfType<GameManager>();
         inventory = gameManager.GetInventory();
         tasks = gameManager.GetTasks();
+        id = SystemInfo.deviceUniqueIdentifier;
     }
 
     private void Start() {
         foreach (Task t in tasks) {
             GameObject tb = Instantiate(taskBox, grid.position, grid.rotation, grid);
             TaskBoxScript tbs = tb.GetComponent<TaskBoxScript>();
-            int currQuantity = inventory.ContainsKey(t.itemRequired) ? inventory[t.itemRequired] : 0; 
+            int currQuantity = inventory.ContainsKey(t.itemRequired) ? inventory[t.itemRequired] : 0;
             tbs.SetItems(t, currQuantity);
             tbs.GetHoverBoxTransform().SetParent(hoverCanvas);
         }
     }
 
     public void NextScene() {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "Scenario 2 End") {
+            StartCoroutine(UploadScore());
+        }
         gameManager.NextScene();
+    }
+
+    IEnumerator UploadScore() {
+
+        Debug.Log("Uploading Score");
+
+        string name = "John Smith";
+        string number = "2018012345";
+
+        int water = 1;
+        int food = 2;
+        int matchsticks = 1;
+        int first_aid_kit = 1;
+        int flashlight = 1;
+        string tasks = $"{{ \"water\": {water}, \"food\": {food}, \"matchsticks\": {matchsticks}, \"first_aid_kit\": {first_aid_kit}, \"flashlight\": {flashlight} }}";
+
+        string data = $"{{ \"name\": \"{name}\", \"student_number\": \"{number}\", \"device_id\": \"{id}\", \"tasks\": {tasks} }}";
+
+        using (UnityWebRequest request = new UnityWebRequest($"{url}/score/upload", "POST")) {
+            byte[] json = new System.Text.UTF8Encoding().GetBytes(data);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(json);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+        }
     }
 }
